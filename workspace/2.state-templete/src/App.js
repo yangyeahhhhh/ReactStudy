6cbb6907125ec6154c1e548dd8811761730ffae0
yangyeahhhhh <112038515+yangyeahhhhh@users.noreply.github.com>
2023-01-12 15:46:09 +0900

    12강

--- workspace/2.state-templete/src/App.js
+++ workspace/2.state-templete/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
-import { Container } from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
 import SearchBar from './components/SearchBar';
+
+import Books from './static_data/Books'
+import BookList from './components/BookList';
 
 class App extends Component {
   render(){
     return(
       <Container>
         <SearchBar></SearchBar>
+        <Grid container spacing={2}>
+          <BookList books={Books}/>
+        </Grid>
       </Container>
     )
   }
--- workspace/2.state-templete/src/components/BookList.js
+++ workspace/2.state-templete/src/components/BookList.js
@@ -1,1 +1,21 @@
 import React, { Component } from 'react';
+import { List } from '@material-ui/core';
+import BookListItem from './BookListItem';
+
+class BookList extends Component {
+  render(){
+    const bookItems = this.props.books.map(book => {
+      return (
+        <BookListItem book = {book} key= {book.ISBN}></BookListItem>
+      )
+    })
+
+    return(
+      <List>
+        {bookItems}
+      </List>
+    )
+  }
+}
+
+export default BookList;
