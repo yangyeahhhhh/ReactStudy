dd2bd5d3a03fa5553b6ecd9f29bef81d6c4eeb86
yangyeahhhhh <112038515+yangyeahhhhh@users.noreply.github.com>
2023-01-12 19:00:34 +0900

    13강

--- workspace/2.state-templete/src/App.js
+++ workspace/2.state-templete/src/App.js
@@ -5,14 +5,29 @@ import SearchBar from './components/SearchBar';
 
 import Books from './static_data/Books'
 import BookList from './components/BookList';
+import BookDetail from './components/BookDetail';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      books : Books,
+      selectedBook : Books[0],
+    }
+  }
   render(){
     return(
       <Container>
         <SearchBar></SearchBar>
         <Grid container spacing={2}>
-          <BookList books={Books}/>
+          <Grid item>
+            <BookList books={this.state.books}/>
+          </Grid>
+          <Grid item>
+          <BookDetail book={ this.state.selectedBook }></BookDetail>
+          </Grid>
         </Grid>
       </Container>
     )
--- workspace/2.state-templete/src/components/BookDetail.js
+++ workspace/2.state-templete/src/components/BookDetail.js
@@ -1,1 +1,37 @@
 import React, { Component } from 'react';
+
+import { Card, CardHeader, CardMedia, CardContent, Typography } from '@material-ui/core';
+
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = {
+  root: {
+    maxWidth : 450,
+  },
+
+  media: {
+    width: '70%',
+    heigh: '70%',
+  }
+}
+
+class BookDetail extends Component {
+  render(){
+
+    const { book, classes } = this.props;
+
+    return(
+      <Card className={classes.root}>
+        <CardHeader title={book.title} subheader={book.author}></CardHeader>
+        <CardMedia className={classes.media} component='img' image={book.imgUrl}></CardMedia>
+        <CardContent>
+          <Typography variant='body2' color='textSecondary' component='p'>
+            {book.introduce}
+          </Typography>
+        </CardContent>
+      </Card>
+    )
+  }
+}
+
+export default withStyles(styles)(BookDetail);
